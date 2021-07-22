3ed68c3e1ea395d44a0c3326876ecd438f1856b6
vadim-trofimenkov-main <vadimtrofimenkov97@gmail.com>
2021-07-22 18:13:52 +0300

    New Feature added: PropertyReader + configuration.properties file

--- src/main/java/utils/Test.java
+++
@@ -1,9 +0,0 @@
-package utils;
-
-public class Test {
-    public static void main(String[] args) {
-        PropertyReader propertyReader = new PropertyReader("src/main/resources/configuration.properties");
-        System.out.println(propertyReader.getPropertyValueByKey("login"));
-        System.out.println(propertyReader.getPropertyValueByKey("blabla"));
-    }
-}
