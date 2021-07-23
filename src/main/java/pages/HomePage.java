f6c6ff83470eaccf0ffdbc170c6b542a63f3fcbd
vadim-trofimenkov-main <vadimtrofimenkov97@gmail.com>
2021-07-23 10:50:23 +0300

    New Feature added: PropertyReader + configuration.properties file

---
+++ src/main/java/pages/HomePage.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+    private WebDriver driver;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+}
--- src/test/java/com/company/base_test/BaseTest.java
+++ src/test/java/com/company/base_test/BaseTest.java
@@ -1,5 +1,36 @@
 package com.company.base_test;
 
-public class BaseTest {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import pages.HomePage;
 
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+    private WebDriver driver;
+    private String link = "https://login.salesforce.com/";
+    public HomePage homepage;
+
+    @BeforeClass
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    @BeforeMethod
+    public void goHome() {
+        driver.get(link);
+    }
+
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
 }
