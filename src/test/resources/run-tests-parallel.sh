b2ef83385d2360330c3815df213770f515ea35f9
dzmitry.rak <dmitryrak11@gmail.com>
2021-08-26 14:22:31 +0300

    Draft for parallelization by docker container

---
+++ src/test/resources/run-tests-parallel.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+NODE_TOTAL=${CIRCLE_NODE_TOTAL:-1}
+NODE_INDEX=${CIRCLE_NODE_INDEX:-0}
+
+i=0
+tests=()
+for file in $(find ./src/test/java/com/seniorlink/tests -name "*Test.java" | sort)
+do
+  if [ $(($i % ${NODE_TOTAL})) -eq ${NODE_INDEX} ]
+  then
+    test=`basename $file | sed -e "s/.java//"`
+    tests+="${test},"
+  fi
+  ((i++))
+done
+
+mvn -Dtest=${tests} test
