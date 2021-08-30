15bfe8b1dde4b9321e11b5b32a3db997fb1a64f2
vadim-trofimenkov-main <vadimtrofimenkov97@gmail.com>
2021-08-30 16:55:35 +0300

    New Feature: Factory + Faker

--- src/test/java/com/itechart/tests/api/CrudApiContactTest.java
+++ src/test/java/com/itechart/tests/api/CrudApiContactTest.java
@@ -10,19 +10,15 @@ public class CrudApiContactTest extends BaseApiTest {
 
     @Test(retryAnalyzer = Retry.class, description = "Create Contact")
     public void createGetUpdateDeleteContact() {
-        Contact contact = new Contact("Mr.", "Contact", "TestApi",
-                "0015g00000P50EYAAZ", "0035g00000CqrWIAAZ", "Test8",
-                "test@tes.t", "testDep", "12945678", "+3751730000", "+3752930000");
+        Contact contact = contactFactory.createNewContact(false);
         ResponseStatus response = contactAdapter.create(contact);
         Assert.assertTrue(response.isSuccess(), "Response is not correct");
-        contact = contactAdapter.get(response.getId());
-        Assert.assertEquals(contact.getName(), "Mr. Contact TestApi", "Response is not correct");
-        Contact updatedContact = new Contact("Mr.", "Contact", "TestApi Updated",
-                "0015g00000P50EYAAZ", "0035g00000CqrWIAAZ", "Test8",
-                "test@tes.t", "testDep", "12945678", "+3751730000", "+3752930000");
-        contactAdapter.update(updatedContact, response.getId());
-        contact = contactAdapter.get(response.getId());
-        Assert.assertEquals(contact.getName(), "Mr. Contact TestApi Updated", "Update request is not correct");
+        Contact receivedContact = contactAdapter.get(response.getId());
+        Assert.assertEquals(receivedContact.getName(), contact.getName(), "Response is not correct");
+        contact = contactFactory.createNewContact(false);
+        contactAdapter.update(contact, response.getId());
+        receivedContact = contactAdapter.get(response.getId());
+        Assert.assertEquals(receivedContact.getName(), contact.getName(), "Update request is not correct");
         contactAdapter.delete(response.getId());
     }
 }
